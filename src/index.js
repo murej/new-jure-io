2a8a1fef8947bc925bbf5693f87f5cf0e7129cee
murej <jmartinec@Jures-MBP.fritz.box>
2017-01-15 18:12:54 +0100

    Initial setup (create-react-app with sass)

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+import './assets/fonts/fonts.css';
+import './index.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
